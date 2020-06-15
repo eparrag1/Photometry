13a51f9414ac2436f01bde3b1509194eddb4439e
eparrag1 <57716059+eparrag1@users.noreply.github.com>
2020-06-15 14:47:03 +0100

    Uploaded first versions of files

---
+++ Class_PSF.py
@@ -0,0 +1,317 @@
+import pandas as pd
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from astropy.wcs import WCS
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.wcs.utils import wcs_to_celestial_frame
+import math
+import os
+import lacosmic
+from scipy import optimize
+from astroquery.vizier import Vizier 
+import astropy.coordinates as coord
+from line_profiler import LineProfiler
+from astropy.stats import sigma_clip
+from scipy import integrate
+from astropy.table import Table
+from photutils.datasets import (make_random_gaussians_table,
+                                 make_noise_image,
+                                 make_gaussian_sources_image)
+from photutils.detection import IRAFStarFinder
+from photutils.psf import IntegratedGaussianPRF, DAOGroup
+from photutils.background import MMMBackground, MADStdBackgroundRMS
+from astropy.modeling.fitting import LevMarLSQFitter
+from astropy.stats import gaussian_sigma_to_fwhm
+from scipy.stats import sigmaclip
+
+
+plt.close('all')
+
+
+class Photimage():
+    
+    def __init__(self,filename,groupid,sky_coord,ja200_coord):
+        self.sky_coord = sky_coord
+        self.ja200_coord =ja200_coord
+        self.groupid = groupid
+        self.hdulist=fits.open(filename)
+        if filename[:2] == 'fz':
+            self.fz = 1
+        else:
+            self.fz = 0
+        self.hdr=self.hdulist[self.fz].header
+        if self.hdr['GROUPID'][:9] != self.groupid:
+            Exception
+        self.limit=3
+        self.sigma_psf=3
+
+        self.Exposure = self.hdr['EXPTIME']
+        if self.hdr['FILTER1'] == 'air':
+            self.Filter =  self.hdr['FILTER']
+            self.MJD = math.floor(self.hdr['MJD-OBS'])
+            self.Telescope = self.hdr['TELESCOP']
+            self.readnoise = self.hdr['RDNOISE']
+        else:
+            self.Filter =  self.hdr['FILTER1']
+            self.MJD = math.floor(self.hdr['MJD'])
+            self.Telescope = 'LT' 
+            self.readnoise = self.hdr['READNOIS']
+        self.gain = self.hdr['GAIN']
+        self.wcs = WCS(self.hdr)
+        
+        if self.Filter == 'B':
+            self.filter_label=1
+            self.Filter_simp = 'B'
+        if self.Filter == 'V':
+            self.filter_label=2
+            self.Filter_simp = 'V'
+        if self.Filter == 'gp':
+            self.filter_label=3
+            self.Filter_simp = 'G'
+        if self.Filter == 'ip':
+            self.filter_label=4
+            self.Filter_simp = 'I'
+        if self.Filter == 'rp':
+            self.filter_label=5
+            self.Filter_simp = 'R'
+        if self.Filter == 'SDSS-I':
+            self.filter_label=6
+            self.Filter_simp = 'I'
+        if self.Filter == 'SDSS-R':
+            self.filter_label=7
+            self.Filter_simp = 'R'
+        if self.Filter == 'SDSS-Z':
+            self.filter_label=8
+            self.Filter_simp = 'Z'
+        if self.Filter == 'SDSS-G':
+            self.filter_label=9
+            self.Filter_simp = 'G'
+        if ('sub' in filename) == True:
+            self.sub = 'Subtracted'
+        else:
+            self.sub = 'Not'
+
+        
+    def xy(self):
+        hpc_frame = wcs_to_celestial_frame(self.wcs)
+        skycoord =  SkyCoord(self.sky_coord, frame=hpc_frame, unit=(u.hourangle, u.deg))
+        ircs =  np.array([[skycoord.ra.deg, skycoord.dec.deg]])
+        coords =  np.array(self.wcs.wcs_world2pix(ircs,1))
+        y =   int((coords[0])[0])
+        x =   int((coords[0])[1])
+        return(x,y)
+           
+    def Flux(self,x,y):
+        x = int(x)
+        y = int(y)
+        r = 25
+        data = self.hdulist[self.fz].data[x-r:x+r,y-r:y+r]
+        data = (lacosmic.lacosmic(data,2,10,10, effective_gain = self.gain, readnoise = self.readnoise))[0]
+        bkgrms = MADStdBackgroundRMS()
+        std = bkgrms(data)
+        iraffind = IRAFStarFinder(threshold=self.limit*std,
+                                   fwhm=self.sigma_psf*gaussian_sigma_to_fwhm,
+                                   minsep_fwhm=0.01, roundhi=5.0, roundlo=-5.0,
+                                   sharplo=0.0, sharphi=2.0)
+        daogroup = DAOGroup(2.0*self.sigma_psf*gaussian_sigma_to_fwhm)
+        mmm_bkg = MMMBackground()
+        psf_model = IntegratedGaussianPRF(sigma=self.sigma_psf)
+        from photutils.psf import IterativelySubtractedPSFPhotometry
+        photometry = IterativelySubtractedPSFPhotometry(finder=iraffind,
+                                                         group_maker=daogroup,
+                                                         bkg_estimator=mmm_bkg,
+                                                         psf_model=psf_model,
+                                                         fitter=LevMarLSQFitter(),
+                                                         niters=1, fitshape=(21,21))
+        
+
+        
+        result_tab = photometry(image=data)    
+        
+        if len(result_tab) > 5:
+            return(0,0) 
+        if len(result_tab) ==0:
+            print('None')
+            return(0,0) 
+        result_tab['Minus'] = np.zeros(len(result_tab))
+        for i in range(len(result_tab)):
+            if 20.5 < result_tab['x_fit'][i] < 28.5 and 20.5 < result_tab['y_fit'][i] < 28.5:
+                result_tab['Minus'][i] = 1
+            else:
+                result_tab['Minus'][i] = 0
+        mask = result_tab['Minus'] == 1.0
+        result_tab = result_tab[mask]
+        if len(result_tab) != 1:
+            return(0,0)   
+        flux_counts = float(result_tab['flux_fit'][0])
+        flux_unc = float(result_tab['flux_unc'][0])
+        flux_unc = flux_unc/flux_counts
+        return(flux_counts,flux_unc)
+
+        
+    def Mags(self):
+
+        if self.Filter == 'B' or self.Filter == 'V':
+            v = Vizier(columns=['RAJ2000', 'DEJ2000','B-V','Bmag','Vmag'])
+            catalog = 'II/336'
+        else:
+            v = Vizier(columns=['RAJ2000', 'DEJ2000','gmag','rmag','imag','zmag'])
+            catalog = 'II/349'
+     
+        result = v.query_region(coord.SkyCoord(ra=self.ja200_coord[0], dec=self.ja200_coord[1],unit=(u.deg, u.deg),frame='icrs'), radius=3*u.arcmin, catalog = catalog)
+        for table_name in result.keys():        
+            table = result[table_name]
+            print(table.colnames)
+
+            if self.Filter == 'B' or self.Filter == 'V':
+                df2 = pd.DataFrame(np.zeros([len(table),5]),columns=['x', 'y', 'B-V', 'B', 'V'])
+            else:
+                df2 = pd.DataFrame(np.zeros([len(table),6]),columns=['x', 'y', 'R', 'Z', 'I', 'G'])
+
+            for i in range(len(table)):
+                ra =  table['RAJ2000'][i]
+                de =  table['DEJ2000'][i]
+                pixa,pixb = self.wcs.wcs_world2pix(ra,de,1)
+                
+                if self.Filter == 'B' or self.Filter == 'V':
+                    new_1 = pd.DataFrame({'x':[pixa]},index = [i])
+                    new_2 = pd.DataFrame({'y':[pixb]},index = [i])
+                    new_3 = pd.DataFrame({'B-V':[table['B-V'][i]]},index = [i])
+                    new_4 = pd.DataFrame({'B':[table['Bmag'][i]]},index = [i])#B-V*(colour_term)
+                    new_5 = pd.DataFrame({'V':[table['Vmag'][i]]},index = [i])
+                    df2.update(new_1)
+                    df2.update(new_2)
+                    df2.update(new_3)
+                    df2.update(new_4)
+                    df2.update(new_5)
+                
+                else:
+                    new_1 = pd.DataFrame({'x':[pixa]},index = [i])
+                    new_2 = pd.DataFrame({'y':[pixb]},index = [i])
+                    new_3 = pd.DataFrame({'R':[table['rmag'][i]]},index = [i])#r-i*(colour_term)
+                    new_4 = pd.DataFrame({'Z':[table['zmag'][i]]},index = [i])#i-z
+                    new_5 = pd.DataFrame({'I':[table['imag'][i]]},index = [i])#i-z*(colour_term)
+                    new_6 = pd.DataFrame({'G':[table['gmag'][i]]},index = [i])#g-r
+                    df2.update(new_1)
+                    df2.update(new_2)
+                    df2.update(new_3)
+                    df2.update(new_4)
+                    df2.update(new_5)
+                    df2.update(new_6)
+        return(df2)    
+        
+
+def ZP(ob):
+    colour_terms = pd.read_csv('/Users/eleonoraparrag/Documents/Python/Photometry/Telescopes.csv')
+    df2 = ob.Mags()
+    zpsum = 0
+    n = 0
+    for i in range(len(df2)):
+        x = df2['x'][i]
+        y = df2['y'][i]    
+
+        count,flux_unc = ob.Flux(y,x)
+        if count == 0:
+            continue
+        else:
+            pass
+        if isinstance(df2[ob.Filter_simp][i], float) == True:
+            pass
+        else:
+            continue
+        if ob.Telescope == '1m0-06':
+            ob.Telescope = '1m0-05'
+        colour = np.float(colour_terms.loc[colour_terms['Telescope'] == ob.Telescope, ob.Filter_simp].iloc[0])
+        measure = -2.5*np.log10(count/ob.Exposure)
+        if ob.Filter_simp == 'B' or ob.Filter_simp == 'V':
+            colour_2 = df2['B-V'][i]
+        if ob.Filter_simp == 'Z': 
+            if isinstance(df2['I'][i], float) == True:
+                colour_2 = df2['Z'][i] - df2['I'][i]
+            else:
+                continue
+        if ob.Filter_simp == 'I':
+            if isinstance(df2['Z'][i], float) == True:
+                colour_2 = df2['Z'][i] - df2['I'][i]
+            else:
+                continue
+        if ob.Filter_simp == 'R':
+            if isinstance(df2['I'][i], float) == True:
+                colour_2 = df2['R'][i] - df2['I'][i]
+            else:
+                continue
+        if ob.Filter_simp == 'G':
+            if isinstance(df2['R'][i], float) == True:
+                colour_2 = df2['G'][i] - df2['R'][i]
+            else:
+                continue           
+        n = n+1
+        zp = df2[ob.Filter_simp][i] - measure - colour*colour_2    
+        if isinstance(zp, float) == True:
+            pass
+        else:
+            continue
+        zpsum = zpsum + zp 
+    if n == 0:
+        return(0)
+    else:
+        return(zpsum/n)
+
+def magnitude(counts,exposure):
+    mag =  2.5*np.log10(counts/exposure)
+    return(mag)
+
+def error(skyerror):
+    err = np.sqrt(((2.5/np.log(10)) * np.sqrt(skyerror))**2 + 0.03**2 + 0.011**2)
+    return(err)
+ 
+def Return(filename,groupid,sky_coord,ja200_coord,cutoff=None):
+    sub = 0
+    ob = Photimage(filename,groupid,sky_coord,ja200_coord) 
+    if ob.sub == 'Subtracted': 
+        sub=1
+        zp = 0
+    else:
+        zp= ZP(ob)
+        if zp == 0:
+            return(0,0,0,0,0,0)
+    x,y = ob.xy()
+    
+    if cutoff is not None and int(ob.MJD) > int(cutoff) and sub == 0:
+        counts,err,mg = 0,0,1
+        
+    else:
+        counts,err = ob.Flux(x,y)
+        
+        
+        if counts == 0:
+            ob.limit = ob.limit - 1
+            ob.sigma_psf = ob.sigma_psf - 0.8
+            counts,err = ob.Flux(x,y)
+            if counts == 0:
+                return(0,0,0,0,0,0)
+        """
+        if counts == 0:
+            ob.limit = ob.limit - 1
+            counts,err = ob.Flux(x,y)
+            if counts == 0:
+                ob.limit = ob.limit - 1
+                ob.sigma_psf = ob.sigma_psf - 0.4
+                counts,err = ob.Flux(x,y)
+                if counts == 0:
+                    ob.limit = ob.limit - 1
+                    ob.sigma_psf = ob.sigma_psf - 0.8
+                    counts,err = ob.Flux(x,y)
+                    if counts == 0:
+                        return(0,0,0,0,0,0)
+        """
+      
+        mg = magnitude(counts,ob.Exposure)
+        err = error(err)
+    filter_label = ob.filter_label
+    return(ob.MJD,mg,zp,err,filter_label,sub)
+
+
+print(Return('/Users/eleonoraparrag/Documents/Python/LCO_combine/ORIG/sub_V_0708.fits', 'SN2019hcc', '21:00:20.930 -21:20:36.06', '315.08720833 -21.34335',58670))
---
+++ Class_PSF_calc.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from astropy.wcs import WCS
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.wcs.utils import wcs_to_celestial_frame
+import math
+import os
+import lacosmic
+from scipy import optimize
+from astroquery.vizier import Vizier 
+import astropy.coordinates as coord
+from line_profiler import LineProfiler
+from astropy.stats import sigma_clip
+from scipy import integrate
+from astropy.table import Table
+from photutils.datasets import (make_random_gaussians_table,
+                                 make_noise_image,
+                                 make_gaussian_sources_image)
+from photutils.detection import IRAFStarFinder
+from photutils.psf import IntegratedGaussianPRF, DAOGroup
+from photutils.background import MMMBackground, MADStdBackgroundRMS
+from astropy.modeling.fitting import LevMarLSQFitter
+from astropy.stats import gaussian_sigma_to_fwhm
+from scipy.stats import sigmaclip
+import operator
+
+import Class_PSF as CP
+plt.close('all')
+
+
+def fitting(directory, groupid, sky_coord, ja200_coord, cutoff = None):
+
+    results_table = Table({'MJD':[],'Filter':[],'Mag':[],'Error':[],'ZP':[],'SUB':[]})
+    for filename in os.listdir(directory):
+        if filename != '.DS_Store':
+            MJD,mg,zp,err,filter_,sub = CP.Return(directory+filename,groupid,sky_coord,(ja200_coord[0],ja200_coord[1]),cutoff)
+            print('MAG',mg)
+            if isinstance(mg, float) and mg != 0.0:
+                print('yes')
+                pass
+            else:
+                continue
+            results_table.add_row([MJD,filter_,mg,err,zp,sub])
+
+    results_table = results_table.group_by(['MJD','Filter','SUB'])
+    results_table = results_table.groups.aggregate(np.mean)
+    
+    df = results_table.to_pandas()
+
+    indices = (df[df['ZP'] == 0].index.to_numpy())
+    
+    for i in indices:
+        print(df['MJD'][i])
+        print(df['Filter'][i])
+
+        zp = np.array(df.loc[(df['MJD'] == df['MJD'][i])&(df['Filter'] == df['Filter'][i])&(df['ZP'] != 0)]['ZP'])
+        zp_idx = np.array(df[(df['MJD'] == df['MJD'][i])&(df['Filter'] == df['Filter'][i])&(df['ZP'] != 0)].index.to_numpy())
+        if len(zp_idx) == 0:
+            print(zp)
+            print('drop')
+            df=df.drop([i])
+        else:   
+            df=df.drop([zp_idx[0]])
+        df['ZP'][i] = zp
+     
+    if cutoff is not None:        
+        df = df[(df['MJD'] < 58670) | (df['SUB'] == 1)]
+    df['Mag'] = df['ZP'] - df['Mag']
+    results_table = df
+    
+    B = results_table[results_table['Filter'] == 1]
+    plt.errorbar(B['MJD'],B['Mag'], yerr = B['Error'], fmt = 'o')
+    scatter = plt.scatter(B['MJD'],B['Mag'],label = 'B')
+    
+    V = results_table[results_table['Filter'] == 2]
+    plt.errorbar(V['MJD'],V['Mag'], yerr = V['Error'], fmt = 'o')
+    plt.scatter(V['MJD'],V['Mag'],label = 'V')
+    
+    gp = results_table[results_table['Filter'] == 3]
+    plt.errorbar(gp['MJD'],gp['Mag'], yerr = gp['Error'], fmt = 'o')
+    plt.scatter(gp['MJD'],gp['Mag'],label = 'gp')
+    
+    ip = results_table[results_table['Filter'] == 4]
+    plt.errorbar(ip['MJD'],ip['Mag'], yerr = ip['Error'], fmt = 'o')
+    plt.scatter(ip['MJD'],ip['Mag'],label = 'ip')
+    
+    rp = results_table[results_table['Filter'] == 5]
+    plt.errorbar(rp['MJD'],rp['Mag'], yerr = rp['Error'], fmt = 'o')
+    plt.scatter(rp['MJD'],rp['Mag'],label = 'rp')
+    
+    I = results_table[results_table['Filter'] == 6]
+    plt.errorbar(I['MJD'],I['Mag'], yerr = I['Error'], fmt = 'o')
+    plt.scatter(I['MJD'],I['Mag'],label = 'SDSS-I')
+    
+    R = results_table[results_table['Filter'] == 7]
+    plt.errorbar(R['MJD'],R['Mag'], yerr = R['Error'], fmt = 'o')
+    plt.scatter(R['MJD'],R['Mag'],label = 'SDSS-R')
+    
+    Z = results_table[results_table['Filter'] == 8]
+    plt.errorbar(Z['MJD'],Z['Mag'], yerr = Z['Error'], fmt = 'o')
+    plt.scatter(Z['MJD'],Z['Mag'],label = 'SDSS-Z')
+    
+    G = results_table[results_table['Filter'] == 9]
+    plt.errorbar(G['MJD'],G['Mag'], yerr = G['Error'], fmt = 'o')
+    plt.scatter(G['MJD'],G['Mag'],label = 'SDSS-G')
+   
+    ax = scatter.axes
+    ax.invert_yaxis()
+    plt.legend()
+    plt.show()
+    
+    plt.savefig('Light_curve')
+    
+    
+#fitting('/Users/eleonoraparrag/documents/Python/LCO_combine/ORIG/', 58670)
+#sky_coord = '21:00:20.930 -21:20:36.06'
+#ja200_coord =(315.08720833,-21.34335)
+
+
+directory = input('Which directory am I looking in? ')
+cutoff = input('If you are using template subtracted images, would you like to clear other data beyond a cutoff? (Optional) ')
+groupid = input('Please enter the object name or groupid as in the fits header ')
+sky_coord = input('Please enter the Galactic coordinates in format "RA DEC" ')
+ja200_coord = input('Please enter the ja200 coordinates in "RA DEC" ')
+fitting(directory,groupid,sky_coord,ja200_coord.split(),cutoff)
+
+
+
+#sky_coord = '02:26:18.55 -09:50:09.0'
+#ja200_coord = (36.57729167, -9.83583333)
+#/Users/eleonoraparrag/documents/Python/SN2019muj/SN_other/'
+#2.46. and it's lookin fixed!
